4264aa5d8757cf50fac7982957cb45376eb3ed77
YELISETTI S R K SURYA CHANDRA RAJESH <121783995+22a91a61j6@users.noreply.github.com>
2025-09-12 11:24:48 +0530

    Update LiveCounts.js

--- src/components/LiveCounts/LiveCounts.js
+++ src/components/LiveCounts/LiveCounts.js
@@ -1,43 +1,10 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment } from "react";
 import Header from "../../components/header/Header";
 import PageTitle from "../../components/pagetitle/PageTitle";
 import Scrollbar from "../../components/scrollbar/scrollbar";
 import Footer from "../../components/footer/Footer";
-import LiveCountSlick from "./LiveCountSlick";
-import axios from "axios";
 
-const StatisticsPage = (props) => {
-  const [liveCounts, setLiveCounts] = useState(0); // State for live counts
-  const [noEvent, setNoEvent] = useState(false);
-
-  const fetchLiveCounts = async () => {
-    try {
-      const response = await axios.get(
-        "http://localhost:3001/count-by-gender?EventDate=YES"
-      );
-      if (response.data && response.data.data) {
-        // Calculate the total count from the response
-        const totalCounts = response.data.data.reduce((acc, item) => acc + item.value, 0);
-        setLiveCounts(totalCounts); // Update live counts
-        setNoEvent(totalCounts === 0); // Set `noEvent` if the total count is 0
-      } else {
-        setLiveCounts(0);
-        setNoEvent(true);
-      }
-    } catch (error) {
-      console.error("Error fetching live counts:", error);
-      setLiveCounts(0);
-      setNoEvent(true);
-    }
-  };
-
-  useEffect(() => {
-    // Fetch live counts on initial render and set an interval for periodic updates
-    fetchLiveCounts();
-    const interval = setInterval(fetchLiveCounts, 10000); // Update every 10 seconds
-    return () => clearInterval(interval); // Cleanup interval on component unmount
-  }, []);
-
+const StatisticsPage = () => {
   return (
     <Fragment>
       <Header hclass={"header--styleFour"} />
@@ -55,7 +22,7 @@ const StatisticsPage = (props) => {
             marginTop: 50,
           }}
         >
-          Live Count {liveCounts}
+          No Live Events At This Moment
         </div>
 
         <div
@@ -63,37 +30,16 @@ const StatisticsPage = (props) => {
             height: 500,
             width: "60%",
             justifySelf: "center",
-            display: "block",
+            display: "flex",
+            alignItems: "center",
             justifyContent: "center",
-            alignItems: "start",
+            fontSize: 30,
+            color: "black",
+            textAlign: "center",
           }}
         >
-          <LiveCountSlick />
+          NO ONGOING EVENTS AT THE MOMENT
         </div>
-        {noEvent && (
-          <div className="headForContact">
-            <span className="sectionTitle__small">
-              <i className="fa-solid fa-user btn__icon"></i>
-              Live Stats
-            </span>
-          </div>
-        )}
-        {noEvent && (
-          <div
-            className="form-container"
-            style={{
-              height: "300px",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              color: "black",
-              fontSize: "30px",
-              wordSpacing: "5px",
-            }}
-          >
-            NO ONGOING EVENTS AT THE MOMENT
-          </div>
-        )}
       </main>
 
       <Footer />
